74bb24a8c6575108c582edd5161b35cffe1b6f0d
Nur-Hamin Mustapha <104641264+npmxdev@users.noreply.github.com>
2025-01-26 17:31:11 +0800

    Update Demo.jsx

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -95,11 +95,11 @@ const Demo = () => {
           ) : (
             article.summary && (
               <div>
-                <h2 className="font-satoshi font-bold text-gray-600 text-xl mb-2">
+                <h2 className="font-satoshi font-bold text-gray-600 text-xl mb-4">
                   Article <span className="font-black bg-gradient-to-r from-blue-600 to-cyan-600 bg-clip-text text-transparent">Summary</span>
                 </h2>
-                <div className="rounded-xl border border-gray-200 bg-white/20 shadow-[inset_10px_-50px_94px_0_rgb(199,199,199,0.2)] backdrop-blur p-4">
-                  <p className="select-all justify-between flex font-inter font-medium text-sm text-gray-700">{article.summary}</p>
+                <div className="rounded-xl border border-gray-200 bg-transparent p-4">
+                  <p className="select-all justify-between items center flex font-inter font-medium text-sm text-gray-700">{article.summary}</p>
                 </div>
               </div>
             )
